b68200bfdb25b8cdaa48b41e971aa02192a8e1d6
Jesse Lopez <lopezjesse@microsoft.com>
2022-10-13 22:34:54 +0000

    Bug fixes + remove micromamba from install script

--- install-opt175.sh
+++ install-opt175.sh
@@ -14,20 +14,14 @@ SRC_DIR=`readlink -f .`
 printf "Downloading and installing software in ${INSTALL_DIR}"
 pushd $INSTALL_DIR
 
-printf "Installing micromamba"
-curl micro.mamba.pm/install.sh | bash
-source /shared/home/hpcadmin/.bashrc
-micromamba create -y -c conda-forge --name fairseq python=3.9
-micromamba activate fairseq
-
 printf "Installing Python requirements"
-pip install -r "${SRC_DIR}/requirements.txt"
+pip install -r "${SRC_DIR}/requirements.txt"  -f https://download.pytorch.org/whl/torch_stable.html
 
 printf "Installing Apex"
 git clone https://github.com/NVIDIA/apex
 pushd apex
 # Avoid CUDA extension + Pytorch complaint.  This is okay on Azure VMs. 
-sed -i "s/(bare_metal_major != torch_binary_major) or (bare_metal_minor != torch_binary_minor)/False/g" setup.py
+sed -i "s/(bare_metal_version != torch_binary_version)/False/g" setup.py
 python -m pip install -v --no-cache-dir --global-option="--cpp_ext" \
     --global-option="--cuda_ext" \
     --global-option="--deprecated_fused_adam" \
@@ -45,7 +39,7 @@ printf "Installing Megatron fork"
 git clone https://github.com/ngoyal2707/Megatron-LM.git
 pushd Megatron-LM
 git checkout fairseq_v2
-pip install -e
+pip install -e .
 popd
 
 printf "Installing Metaseq"
@@ -59,7 +53,7 @@ popd
 printf "Installing Fairscale"
 git clone https://github.com/facebookresearch/fairscale.git
 pushd fairscale
-git checkout fixing_memory_issues_with_keeping_overla
+git checkout fixing_memory_issues_with_keeping_overlap
 pip install .
 popd 
 
